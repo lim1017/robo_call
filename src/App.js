101b2af87682d4ef37b3b7b0d108aee9c76a35f2
tom <t.lim@rockmasstech.com>
2022-04-04 02:14:19 -0400

    new twilio number, update rest later

--- src/App.js
+++ src/App.js
@@ -4,56 +4,62 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row } from "react-bootstrap";
 import FormSettings from "./components/FormSettings/FormSettings.jsx";
 
-
 require("dotenv").config();
 
 const accountSid = process.env.REACT_APP_SID;
 const authToken = process.env.REACT_APP_TOKEN;
+
+console.log(authToken);
+
 var twilio = require("twilio");
 const client = new twilio(accountSid, authToken);
 
-
 function call(phone, msg, audio) {
+  if (!phone) {
+    alert("please enter phone #");
+  }
 
   //cant sent spaces in https requests so replace spaces with +
   const newMsg = msg.replace(/ /g, "+");
 
+  let song = "";
 
-  let song=''
-
-  if (audio ==="Rick Roll"){
-    song='http://demo.twilio.com/docs/classic.mp3'
+  if (audio === "Rick Roll") {
+    song = "http://demo.twilio.com/docs/classic.mp3";
   }
 
-  if (audio ==="Barrel"){
-    song='https://melon-zebra-7147.twil.io/assets/BARRELROLL.mp3'
+  if (audio === "Barrel") {
+    song = "https://melon-zebra-7147.twil.io/assets/BARRELROLL.mp3";
   }
 
-  if (audio ==="Just Do It"){
-    song='https://melon-zebra-7147.twil.io/assets/DOIT.mp3'
+  if (audio === "Just Do It") {
+    song = "https://melon-zebra-7147.twil.io/assets/DOIT.mp3";
   }
-
 
   client.calls
     .create({
-      url:
-        `https://handler.twilio.com/twiml/EH34f76e7cba60708c9daa84db1b77a062?MSG=${newMsg}&song=${song}`,
+      url: `https://handler.twilio.com/twiml/EH34f76e7cba60708c9daa84db1b77a062?MSG=${newMsg}&song=${song}`,
       to: phone,
-      from: "+13656571048",
+      from: "+12269185064",
     })
-    .then((call) => console.log(call.sid));
+    .then((call) => console.log(call.sid))
+    .catch((err) => console.log(err));
 }
 
 function text(phone, msg, img) {
+  if (!phone) {
+    alert("please enter phone #");
+  }
 
   client.messages
     .create({
       body: msg,
       mediaUrl: [img],
       to: phone,
-      from: "+13656571048",
+      from: "+12269185064",
     })
-    .then((message) => console.log(message.sid));
+    .then((message) => console.log(message.sid))
+    .catch((err) => console.log(err));
 }
 
 function App() {
@@ -63,11 +69,9 @@ function App() {
         <h1>ROBO CALL SITE!!</h1>
       </Row>
 
-    <Row className="justify-content-md-center">
-      <FormSettings call={call} text={text} />
-    </Row>
-
-    
+      <Row className="justify-content-md-center">
+        <FormSettings call={call} text={text} />
+      </Row>
     </Container>
   );
 }
--- src/components/FormSettings/FormSettings.jsx
+++ src/components/FormSettings/FormSettings.jsx
@@ -1,50 +1,46 @@
-import React, { useState } from "react";
-import { Form, Row } from 'react-bootstrap';
+import React from "react";
+import { Form, Row } from "react-bootstrap";
 import Button from "@material-ui/core/Button";
 
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-
-import NativeSelect from '@material-ui/core/NativeSelect';
-import InputBase from '@material-ui/core/InputBase';
-
+import InputBase from "@material-ui/core/InputBase";
+import { Typography } from "@material-ui/core";
 
 const BootstrapInput = withStyles((theme) => ({
   root: {
-    'label + &': {
+    "label + &": {
       marginTop: theme.spacing(3),
     },
   },
   input: {
     borderRadius: 4,
-    position: 'relative',
+    position: "relative",
     backgroundColor: theme.palette.background.paper,
-    border: '1px solid #ced4da',
+    border: "1px solid #ced4da",
     fontSize: 16,
-    padding: '10px 26px 10px 12px',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    padding: "10px 26px 10px 12px",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
     // Use the system font instead of the default Roboto font.
     fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
+      "-apple-system",
+      "BlinkMacSystemFont",
       '"Segoe UI"',
-      'Roboto',
+      "Roboto",
       '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
+      "Arial",
+      "sans-serif",
       '"Apple Color Emoji"',
       '"Segoe UI Emoji"',
       '"Segoe UI Symbol"',
-    ].join(','),
-    '&:focus': {
+    ].join(","),
+    "&:focus": {
       borderRadius: 4,
-      borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+      borderColor: "#80bdff",
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
     },
   },
 }))(InputBase);
@@ -59,124 +55,117 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-
-
-
-
-
 export default function FormSettings(props) {
-
-
   const [phone, setPhone] = React.useState("");
-  const [msg, setMsg] = React.useState("");
-  const [img, setImg] = React.useState("");
+  const [msg, setMsg] = React.useState("Hello there!");
+  const [img, setImg] = React.useState(
+    "https://i.pinimg.com/564x/ac/df/3d/acdf3da0fdbb3cf56cffcda4c87d8292.jpg"
+  );
   const classes = useStyles();
-  const [audio, setAudio] = React.useState('');
+  const [audio, setAudio] = React.useState("Rick Roll");
 
+  function handleChangePhone(event) {
+    setPhone(event.target.value);
+  }
 
+  const handleChangeMsg = (event) => {
+    setMsg(event.target.value);
+  };
 
-function handleChangePhone(event){
-  setPhone(event.target.value);
-}
+  const handleChangeImg = (event) => {
+    setImg(event.target.value);
+  };
 
-const handleChangeMsg = event => {
-  setMsg(event.target.value);
-};
-
-const handleChangeImg = event =>{
-  setImg(event.target.value);
-}
-
-const handleChangeAudio = event =>{
-  setAudio(event.target.value);
-  console.log(audio)
-}
-
+  const handleChangeAudio = (event) => {
+    setAudio(event.target.value);
+  };
 
   return (
-  <Form>
-    <Form.Group controlId="formBasicEmail">
-      <Form.Label>Phone # (Include country code CAN/US=1)</Form.Label>
-      
-      <Form.Control type="phonw" placeholder="Enter Phone #" onChange={handleChangePhone}
-      />
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Phone # (Include country code CAN/US=1)</Form.Label>
 
-      <Form.Text className="text-muted">
-        enter phone
-      </Form.Text>
-    </Form.Group>
+        <Form.Control
+          type="phone"
+          placeholder="Enter Phone #"
+          onChange={handleChangePhone}
+        />
+      </Form.Group>
 
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Message</Form.Label>
+        <Form.Control
+          type="message"
+          placeholder="Default: Hello there!"
+          onChange={handleChangeMsg}
+        />
+      </Form.Group>
 
-    <Form.Group controlId="formBasicPassword">
-      <Form.Label>Message</Form.Label>
-      <Form.Control type="message" placeholder="Message"
-      onChange={handleChangeMsg}
-      />
-    </Form.Group>
-    
+      <Row className="d-flex flex-column align-items-center mt-4 mb-4">
+        <img
+          src="https://freesvg.org/img/arrowdownred.png"
+          width={150}
+          height={150}
+        />
+        <Typography>Choose a path</Typography>
+      </Row>
 
-<Row>
-    
-    <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-label">Audio: Call Only</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={audio}
-          onChange={handleChangeAudio}
-          defaultValue='Rick Roll'
+      <Row>
+        <Form.Group
+          controlId="formBasicPassword"
+          className="d-flex flex-column"
         >
-          <MenuItem value='Rick Roll'>Rick Roll</MenuItem>
-          <MenuItem value='Barrel'>Do a Barrel Roll</MenuItem>
-          <MenuItem value='Just Do It'>Just Do It</MenuItem>
-        </Select>
+          <Form.Label>Audio Clip: Calls Only</Form.Label>
 
-        {/* <InputLabel htmlFor="demo-customized-select-native"></InputLabel>
-        
-        <NativeSelect
-          id="demo-customized-select-native"
-          value={audio}
-          onChange={handleChangeAudio}
-          input={<  BootstrapInput />}
+          <FormControl className={classes.formControl}>
+            <Select
+              style={{ height: 38 }}
+              variant="outlined"
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={audio}
+              onChange={handleChangeAudio}
+            >
+              <MenuItem value="Rick Roll">Rick Roll</MenuItem>
+              <MenuItem value="Barrel">Do a Barrel Roll</MenuItem>
+              <MenuItem value="Just Do It">Just Do It</MenuItem>
+            </Select>
+          </FormControl>
+        </Form.Group>
+
+        <Form.Group style={{ marginLeft: "5em" }} controlId="formBasicPassword">
+          <Form.Label style={{ marginBottom: "15px" }}>
+            Image URL: Text only
+          </Form.Label>
+          <Form.Control
+            type="url"
+            placeholder="Default: Fat Cat"
+            onChange={handleChangeImg}
+          />
+        </Form.Group>
+      </Row>
+
+      <Row className="justify-content-md-center">
+        <Button
+          style={{ marginRight: "3em" }}
+          variant="contained"
+          color="primary"
+          onClick={() => props.call(phone, msg, audio)}
         >
-          <option aria-label="None" value="" />
-          <option value={10}>Rick Roll</option>
-          <option value={20}>Do A Barrel Roll</option>
-          <option value={30}>Just Do It!</option>
-        </NativeSelect> */}
-    </FormControl>
-
-    
-    
-    
-    
-    
-    <Form.Group style={{marginLeft:'3em'}} controlId="formBasicPassword">
-      <Form.Label>Image: Text only</Form.Label>
-      <Form.Control type="url" placeholder="Img Url"
-      onChange={handleChangeImg}
-      />
-    </Form.Group>
-
-
-</Row>
-
-
-
-    <Row className="justify-content-md-center">
-        <Button style={{marginRight:'3em'}} variant="contained" color="primary" onClick={()=>props.call(phone, msg, audio)}>
           CALL
         </Button>
 
-        <Button style={{marginLeft:'3em'}} variant="contained" color="primary" onClick={()=>props.text(phone, msg, img)}>
+        <Button
+          style={{ marginLeft: "3em" }}
+          variant="contained"
+          color="primary"
+          onClick={() => props.text(phone, msg, img)}
+        >
           PICTURE TEXT
         </Button>
-    </Row>
-
-  </Form>
-
-  )
+      </Row>
+    </Form>
+  );
 }
 
 // export default FormSettings
